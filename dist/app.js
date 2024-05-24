f39cfc87a8b476fe58459403d7e13a71dbff5863
Arthur Kozub <kozub0246@gmail.com>
2024-05-24 22:00:48 +0300

    feat: posts logic implementation

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
 const db_1 = __importDefault(require("./db"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -12,12 +13,10 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 app.use(express_1.default.json());
 app.use('/users', userRoutes_1.default);
+app.use('/posts', postRoutes_1.default);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
-});
-app.get('/', (req, res) => {
-    res.send('The app is working, keep grinding!');
 });
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import userRoutes from './routes/userRoutes';
+import postRoutes from './routes/postRoutes';
 import connectDB from './db';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
